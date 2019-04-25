73af73b0e9b7c355de0013e4fa28f561d413422e
SicktorHass <igor.work@outlook.de>
2019-04-25 13:53:55 +0200

    initial commit: \n*** added virtual environment \n*** project skeletton

---
+++ src/ray_tracer_main.py
@@ -0,0 +1,5 @@
+from vectors/ray.py import Ray
+from vectors/point import Point
+
+if __name__ == "__main__":
+    ray = Ray('test', 'test2')
---
+++ src/vectors/ray.py
@@ -0,0 +1,13 @@
+#import numpy as np
+
+class Ray(object):
+
+
+    def __init__(self, origin, direction):
+        pass
+
+    def __repr__(self):
+        print('Hello World')
+
+    def castRay(self):
+        pass
